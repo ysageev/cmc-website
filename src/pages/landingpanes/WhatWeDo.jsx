eff716cae260a379d381896d3c76e86748af525d
ysageev <ysageev@yahoo.com>
2025-02-03 10:11:18 -0500

    Adjust text wrap

--- src/pages/landingpanes/WhatWeDo.jsx
+++ src/pages/landingpanes/WhatWeDo.jsx
@@ -1,50 +1,24 @@
 function WhatWeDo() {
 	return (
 		<div style={{ marginLeft: 30 }}>
-			<span style={{ fontSize: "2em", fontWeight: "bold" }}>
-				Our services...
-			</span>
+			<span style={{ fontSize: "2em", fontWeight: "bold" }}>Our services...</span>
 			<br />
 			<br />
 			<div style={{ display: "flex", flexWrap: "wrap" }}>
-				<ServicesListItem
-					icon="fa-helmet-safety"
-					title="Construction Risk Management"
-					details="Minimize Risk. Maximize Confidence"
-				/>
+				<ServicesListItem icon="fa-helmet-safety" title="Construction Risk Management" details="Minimize Risk. Maximize Confidence" />
 				<br />
-				<ServicesListItem
-					icon="fa-building-magnifying-glass"
-					title="Property Inspection and Due Diligence"
-					details="Inspect with Precision.  Invest with Confidence."
-				/>
+				<ServicesListItem icon="fa-building-magnifying-glass" title="Property Inspection and Due Diligence" details="Inspect with Precision.  Invest with Confidence." />
 				<br />
-				<ServicesListItem
-					icon="fa-chart-mixed-up-circle-dollar"
-					title="Data Analytics and Visualization"
-					details="Best decisions backed by data."
-				/>
+				<ServicesListItem icon="fa-chart-mixed-up-circle-dollar" title="Data Analytics and Visualization" details="Best decisions backed by data." />
 				<br />
-				<ServicesListItem
-					icon="fa-suitcase"
-					title="Portfolio Management"
-					details="Master your portfolio. Maximize the potential."
-				/>
+				<ServicesListItem icon="fa-suitcase" title="Portfolio Management" details="Master your portfolio. Maximize the potential." />
 				<br />
-				<ServicesListItem
-					icon="fa-badge-check"
-					title="Completion Certificates and Closeout"
-					details="Certified Success.  Close with Certainty."
-				/>
+				<ServicesListItem icon="fa-badge-check" title="Completion Certificates and Closeout" details="Certified Success.  Close with Certainty." />
 				<br />
-				<ServicesListItem
-					icon="fa-landmark-flag"
-					title="Historic Tax Credit Monitoring"
-					details="Track Tax Credits."
-				/>
+				<ServicesListItem icon="fa-landmark-flag" title="Historic Tax Credit Monitoring" details="Track Tax Credits." />
 			</div>
 		</div>
-	);
+	)
 }
 
 function ServicesListItem({ icon, title, details }) {
@@ -66,10 +40,10 @@ function ServicesListItem({ icon, title, details }) {
 				}}
 			/>
 			<div style={{ marginLeft: 20, lineHeight: 1.4 }}>
-				<div style={{ fontWeight: "bold", fontSize: "1.1em" }}>{title}</div>
+				<div style={{ fontWeight: "bold", fontSize: "1.1em", textWrap: "pretty" }}>{title}</div>
 				<div style={{ fontSize: ".7em" }}>{details}</div>
 			</div>
 		</div>
-	);
+	)
 }
-export default WhatWeDo;
+export default WhatWeDo
