1b4ffb0b56c574bff668c62570c2d64e7e98ba76
ysageev <ysageev@yahoo.com>
2025-02-06 13:19:13 -0500

    Testing styled-components compatibility

--- src/pages/landingpanes/Hero.jsx
+++ src/pages/landingpanes/Hero.jsx
@@ -1,75 +1,23 @@
 import Link from "@docusaurus/Link"
 import CompaniesUsing from "@site/src/components/CompaniesUsing"
 import CMCLogoLarge from "@site/src/components/common/CMCLogoLarge"
-import styled from "styled-components"
 
-const PageHeader = styled.div`
-	background-image: url("img/hero-background.png");
-	background-size: cover;	
-	background-repeat: no-repeat;
-	padding-bottom: 30px;
-`
-
-const PageHeaderText = styled.div`
-	font-family: Lexend, sans-serif;
-	font-size: 1.8em;
-	font-weight: bold;
-	line-height: 1.3em;
-	padding-top: 20px;
-	padding-right: 10px;
-	margin-left: -5px;
-	
-`
-
-const PageHeaderDetailsText = styled.div`
-	font-family: Lexend, sans-serif;
-	font-weight: normal;
-	font-size: 1.2em;
-	line-height: 1.3em;
-	padding-top: 20px;
-	margin-bottom: 20px;
-	padding-right: 20px;
-	text-wrap: pretty;
-`
-
-const PageBody = styled.div`
-	display: flex;
-	align-items: center;
-	flex-wrap: wrap;
-	padding-bottom: 20px;
-	padding-left: 30px;
-`
-const PageLinkContainer = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	padding-bottom: 20px;
-	height: 60px;
-	gap: 10px;
-	margin: 10px;
-`
-const TickerContainer = styled.div`
-	clear: both;
-	height: 50px;
-	width: 100%;
-	margin-top: 20px;
-	margin-bottom: 20px;
-`
 //TODO: NAMING AND HIERARCHY IS WRONG WRT STYLES
 //_____________________________________________
 function Hero() {
 	return (
-		<PageHeader>
+		<div className="page-header">
 			<CMCLogoLarge style={{ paddingRight: 40 }} />
-			<PageBody>
-				<PageHeaderText>Reduce Risk, Enhance Decisions, Get Results</PageHeaderText>
-				<PageHeaderDetailsText>
+			<div className="page-body">
+				<div className="page-header-text">Reduce Risk, Enhance Decisions, Get Results</div>
+				<div className="page-header-details-text">
 					For more than 35 years, CMC has served <strong>banks</strong> and <strong>investors</strong> in
 					<strong>&nbsp;commercial construction.</strong>
 					<br />
 					<br />
 					We empower our clients to understand and mitigate construction project risks.
-				</PageHeaderDetailsText>
-				<PageLinkContainer>
+				</div>
+				<div className="page-links">
 					{/* <Link className="button button--secondary button--lg" to="/services">
 						Learn More
 					</Link> */}
@@ -77,13 +25,13 @@ function Hero() {
 						Video <i className="fa-duotone fa-solid fa-video" />
 					</Link>
 					<br />
-				</PageLinkContainer>
-			</PageBody>
+				</div>
+			</div>
 
-			<TickerContainer>
+			<div style={{ height: 50 }}>
 				<CompaniesUsing />
-			</TickerContainer>
-		</PageHeader>
+			</div>
+		</div>
 	)
 }
 export default Hero
