f66c00af0a7edb1a88c530196de2ef0170d4c687
ysageev <ysageev@yahoo.com>
2025-02-12 16:10:22 -0500

    Page ordering, layout tweaks

--- src/pages/landingpanes/WhoWeHelp.jsx
+++ src/pages/landingpanes/WhoWeHelp.jsx
@@ -49,7 +49,7 @@ const ICON_STYLE = {
 
 function EntityItem({ label }) {
 	return (
-		<div style={{ display: "flex", alignItems: "center", padding: 5, fontSize: "1.2em", fontFamily: "Lexend, sans-serif" }}>
+		<div style={{ display: "flex", alignItems: "center", padding: 5, fontSize: "1.1em", fontFamily: "Lexend, sans-serif" }}>
 			<div>
 				<i className={ICON} style={ICON_STYLE} />
 			</div>
