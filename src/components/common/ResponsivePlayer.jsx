ac2324a721704d9f2c715dc8bbd13f6a5f92818e
ysageev <ysageev@yahoo.com>
2025-02-13 15:18:53 -0500

    Remove react-player

--- src/components/common/ResponsivePlayer.jsx
+++ src/components/common/ResponsivePlayer.jsx
@@ -1,11 +1,7 @@
-import ReactPlayer from "react-player"
+// import ReactPlayer from "react-player"
 
 const ResponsivePlayer = (props) => {
-	return (
-		<div className="player-wrapper">
-			<ReactPlayer className="react-player" width="100%" height="100%" controls {...props} />
-		</div>
-	)
+	return <div className="player-wrapper">{/* <ReactPlayer className="react-player" width="100%" height="100%" controls {...props} /> */}</div>
 }
 
 export default ResponsivePlayer
