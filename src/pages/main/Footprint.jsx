3d59dd1409c1c649edb09d27aa350d286ae8953e
ysageev <ysageev@yahoo.com>
2025-03-13 12:41:25 -0400

    Inline to CSS

--- src/pages/main/Footprint.jsx
+++ src/pages/main/Footprint.jsx
@@ -17,13 +17,13 @@ export default Footprint
 const ServiceAreaGraphic = () => {
     return (
         <div>
-            <img src="/img/about/service-map-bare.png" style={{ maxWidth: 300 }} />
+            <img src="/img/about/service-map-bare.png" className="service-map" />
 
-            <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", fontSize: "clamp(8pt, .8vw, 12pt" }}>
-                <div style={{ display: "flex", alignItems: "center" }}>
+            <div className="service-area-info">
+                <div className="service-area-item">
                     <ColorChip clr="#D9782E" /> Secondary Market
                 </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
+                <div className="service-area-item">
                     <ColorChip clr="#47B2E0" /> Active Market
                 </div>
             </div>
@@ -32,5 +32,5 @@ const ServiceAreaGraphic = () => {
 }
 
 const ColorChip = ({ clr }) => {
-    return <div style={{ width: 20, height: 20, border: "1px solid gray", borderRadius: 2, padding: 5, margin: 5, backgroundColor: clr }} />
+    return <div className="color-chip" style={{ backgroundColor: clr }} />
 }
