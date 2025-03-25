39c6542c6765e893dfb3f6be2e0b6ba4a816f71e
ysageev <ysageev@yahoo.com>
2025-03-25 14:04:13 -0400

    Trying to fix layout of consultation, take 3

--- src/pages/main/RotatingClients.jsx
+++ src/pages/main/RotatingClients.jsx
@@ -1,19 +1,12 @@
 import "./css/clients.css"
 import RotatingText from "@site/src/components/common/RotatingText.jsx"
 
-const ICON = "fa-duotone fa-solid fa-badge-check fa-xl fa-fw"
-const ICON_STYLE = {
-    "--fa-primary-color": "#47B2E0",
-    "--fa-secondary-color": "#47B2E0",
-}
-
 const clientsArr = ["National Banks", "Regional Banks", "Community Banks", "Credit Unions", "Nonprofit Lenders", "Pension Funds", "Institutional Investors", "Municipalities", "Mortgage Companies"]
 
 function RotatingClients() {
     return (
         <div className="clients-rotate-static">
-            {/* <i className={ICON} /> */}
-            We serve:&nbsp;&nbsp;
+            <div className="consultation-label">We serve:</div>
             <RotatingText
                 texts={clientsArr}
                 mainClassName="clients-rotate-main"
@@ -26,17 +19,6 @@ function RotatingClients() {
                 transition={{ type: "spring", damping: 30, stiffness: 400 }}
                 rotationInterval={2200}
             />
-        </div>
-    )
-}
-
-function EntityItem({ label }) {
-    return (
-        <div className="clients-entity">
-            <div>
-                <i className={ICON} style={ICON_STYLE} />
-            </div>
-            <div>&nbsp;{label}</div>
         </div>
     )
 }
