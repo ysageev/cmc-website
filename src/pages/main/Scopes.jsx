a568b581dc00b15f051bed5d1dc9e003a0de6d9a
ysageev <ysageev@yahoo.com>
2025-03-27 11:43:27 -0400

    Add new project image

--- src/pages/main/Scopes.jsx
+++ src/pages/main/Scopes.jsx
@@ -3,7 +3,10 @@ import "./css/advantages.css"
 function Scopes() {
     return (
         <div className="adv-subcontainer">
-            <img src="/img/main/cranes.jpg" className="adv-img" />
+            <img
+                src="/img/main/project-min.png"
+                className="adv-img"
+            />
             <div className="adv-blurb">
                 <div className="adv-blurb-label">Any project</div>
                 From hard cost budgets of $1M to over $100M, CMC provides the same outstanding features and quality, all at fees that meet or beat the competition.
