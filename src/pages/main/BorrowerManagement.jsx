ba7aa145f329737428b1e6cfbf60dc13df33fcb2
ysageev <ysageev@yahoo.com>
2025-03-27 10:57:23 -0400

    Anthony's AI images for advantages

--- src/pages/main/BorrowerManagement.jsx
+++ src/pages/main/BorrowerManagement.jsx
@@ -3,7 +3,10 @@ import "./css/advantages.css"
 function BorrowerManagement() {
     return (
         <div className="adv-subcontainer">
-            <img src="/img/main/missing-doc.jpg" className="adv-img" />
+            <img
+                src="/img/main/admin-min.png"
+                className="adv-img"
+            />
             <div className="adv-blurb">
                 <div className="adv-blurb-label">Stakeholder Administration</div>
                 Other vendors force you to chase after stakeholders to obtain documents and signatures. CMC doesn't just recommend that specific documents be provided -- we track them and get them.
--- src/pages/main/EliteReporting.jsx
+++ src/pages/main/EliteReporting.jsx
@@ -3,7 +3,10 @@ import "./css/advantages.css"
 function EliteReporting() {
     return (
         <div className="adv-subcontainer">
-            <img src="/img/main/sv-rep-cover.png" className="adv-img" />
+            <img
+                src="/img/main/reporting-min.png"
+                className="adv-img"
+            />
             <div className="adv-blurb">
                 <div className="adv-blurb-label">Exceptional Reporting</div>
                 Our comprehensive reports are valued throughout the industry for their superior quality and features.
