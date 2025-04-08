a7f8a041c3ab6e792da22006be1ebd342b513586
ysageev <ysageev@yahoo.com>
2025-04-08 13:18:13 -0400

    Deploy mermaid diagram of CDP

---
+++ docs/public/cdp/intro.md
@@ -0,0 +1,57 @@
+---
+sidebar_position: 1
+---
+
+# CDP Flow Diagram
+
+```mermaid
+
+graph LR
+
+
+
+A@{label: "
+<div >
+    <div style='font-weight: bold;text-wrap:wrap;'>Monthly Construction Draw Meeting</div>
+    <div style='text-wrap: wrap;text-align:left;font-size:12pt;'>
+        The preference is for the Owner, Lender’s Construction Consultant, Architect and Contractor all be present for the monthly draw meeting. Consultant will review Application for Payment, all RFI and submittal logs, discuss schedule with upcoming milestone dates, manpower, testing reports, anticipated cost reports and any potential or pending change orders (PCO).<br/><br/>
+    </div>
+</div>"}
+
+A==>B
+
+
+B@{label: "
+<div style='text-wrap: wrap;text-align:left;font-size:12pt;'>
+  Consultant must receive fully executed and notarized copies of G702 and G703 (as applicable), lien waivers (must beunconditional for previous payments and conditional for current request) and all approved change orders.<br/><br/>
+ </div>"}
+
+B==>C
+
+C@{label: "
+  <div style='text-wrap: wrap;text-align:left;font-size:12pt;'>
+    Borrower/Owner assembles the monthly draw package, which includes all hard and soft costs for the current period. Lender requires all invoices, paid receipts, purchase orders and similar documents to be included in the draw package. Monthly draw may be submitted regular or electronic mail.<br/><br/>
+    </div>"}
+
+C==>D
+
+D@{label: "
+<div style='text-wrap: wrap;text-align:left;font-size:12pt;'>
+  Consultant reviews hard cost draw documentation and produces a Site Visit Report for Lender certifying all project hard costs. Site Visit Report is submitted to Lender for review.<br/><br/>
+</div>"}
+
+D==>E
+
+E@{label: "
+<div style='text-wrap: wrap;text-align:left;font-size:12pt;'>
+  Site Visit Report and draw package information is reviewed internally by the **Lender** and all necessary requests for additional supporting documents will be made of the Owner, Contractor and Architect. Lender will order title update and arrange for funding of draw request. Lender typically has 10 business days to fund the draw after a complete draw package has been provided.<br/><br/>
+</div>"}
+
+E==>F
+
+F@{label: "
+<div style='text-wrap: wrap;text-align:left;font-size:12pt;'>
+  Upon receipt of clear title, funds are wired/transferred to the Borrower account. In the case any liens appear on the title continuation report, prior to advancing funds; it is the responsibility of the Borrower and/or Contractor to remedy per the Lender’s requirements which may encompass bonding over or providing the appropriate collateral with the title company.<br/><br/>
+</div>"}
+
+```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,151 +10,154 @@ import { themes as prismThemes } from "prism-react-renderer"
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "CMC",
-	tagline: "Construction Monitoring Consultants",
-	favicon: "img/favicon.ico",
+    title: "CMC",
+    tagline: "Construction Monitoring Consultants",
+    favicon: "img/favicon.ico",
 
-	// Set the production url of your site here
-	url: "https://go-cmc.com",
-	// Set the /<baseUrl>/ pathname under which your site is served
-	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+    // Set the production url of your site here
+    url: "https://go-cmc.com",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/",
 
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "ysageev", // Usually your GitHub org/user name.
-	projectName: "cmc-website", // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "ysageev", // Usually your GitHub org/user name.
+    projectName: "cmc-website", // Usually your repo name.
 
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
 
-	// Even if you don't use internationalization, you can use this field to set
-	// useful metadata like html lang. For example, if your site is Chinese, you
-	// may want to replace "en" with "zh-Hans".
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en"],
-	},
-
-	presets: [
-		[
-			"classic",
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: {
-					sidebarPath: "./sidebars.js",
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					//editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-				},
-				blog: {
-					showReadingTime: true,
-					feedOptions: {
-						type: ["rss", "atom"],
-						xslt: true,
-					},
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-					// Useful options to enforce blogging best practices
-					onInlineTags: "warn",
-					onInlineAuthors: "warn",
-					onUntruncatedBlogPosts: "warn",
-				},
-				theme: {
-					customCss: "./src/css/custom.css",
-				},
-			}),
-		],
-	],
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			// Replace with your project's social card
-			docs: { sidebar: { autoCollapseCategories: true } },
-			image: "img/cmc-icon.svg",
-			navbar: {
-				title: "CMC",
-				logo: {
-					alt: "Construction Monitoring Consultants",
-					src: "img/cmc-icon.svg",
-				},
-				items: [
-					{
-						to: "about",
-						label: "About",
-						position: "left",
-					},
-					{
-						to: "services",
-						label: "Services",
-						position: "left",
-					},
-					{
-						to: "contact",
-						label: "Contact",
-						position: "left",
-					},
-					{
-						type: "docSidebar",
-						sidebarId: "resourcesPublicSidebar",
-						position: "left",
-						label: "Resources",
-					},
-					// {
-					// 	type: "docSidebar",
-					// 	sidebarId: "resourcesPrivateSidebar",
-					// 	position: "left",
-					// 	label: "Surveyor",
-					// },
-				],
-			},
-			footer: {
-				style: "dark",
-				links: [
-					{
-						title: "Directory",
-						items: [
-							{
-								label: "Contact",
-								href: "/contact",
-							},
-							{
-								label: "About",
-								href: "/about",
-							},
-							{
-								label: "Services",
-								href: "/services",
-							},
-							{
-								label: "Resources",
-								href: "/docs/public/intro",
-							},
-						],
-					},
-					{
-						title: "Legal",
-						items: [
-							{
-								label: "Terms of Service",
-								href: "/tos",
-							},
-							{
-								label: "Privacy Statement",
-								href: "/privacy",
-							},
-						],
-					},
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} CMC, Inc.`,
-			},
-			prism: {
-				theme: prismThemes.github,
-				darkTheme: prismThemes.dracula,
-			},
-			scripts: ["https://kit.fontawesome.com/9201d8e0d6.js"],
-		}),
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
+    markdown: {
+        mermaid: true,
+    },
+    themes: ["@docusaurus/theme-mermaid"],
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: "./sidebars.js",
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    //editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ["rss", "atom"],
+                        xslt: true,
+                    },
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    // Useful options to enforce blogging best practices
+                    onInlineTags: "warn",
+                    onInlineAuthors: "warn",
+                    onUntruncatedBlogPosts: "warn",
+                },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            }),
+        ],
+    ],
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            docs: { sidebar: { autoCollapseCategories: true } },
+            image: "img/cmc-icon.svg",
+            navbar: {
+                title: "CMC",
+                logo: {
+                    alt: "Construction Monitoring Consultants",
+                    src: "img/cmc-icon.svg",
+                },
+                items: [
+                    {
+                        to: "about",
+                        label: "About",
+                        position: "left",
+                    },
+                    {
+                        to: "services",
+                        label: "Services",
+                        position: "left",
+                    },
+                    {
+                        to: "contact",
+                        label: "Contact",
+                        position: "left",
+                    },
+                    {
+                        type: "docSidebar",
+                        sidebarId: "resourcesPublicSidebar",
+                        position: "left",
+                        label: "Resources",
+                    },
+                    // {
+                    // 	type: "docSidebar",
+                    // 	sidebarId: "resourcesPrivateSidebar",
+                    // 	position: "left",
+                    // 	label: "Surveyor",
+                    // },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Directory",
+                        items: [
+                            {
+                                label: "Contact",
+                                href: "/contact",
+                            },
+                            {
+                                label: "About",
+                                href: "/about",
+                            },
+                            {
+                                label: "Services",
+                                href: "/services",
+                            },
+                            {
+                                label: "Resources",
+                                href: "/docs/public/intro",
+                            },
+                        ],
+                    },
+                    {
+                        title: "Legal",
+                        items: [
+                            {
+                                label: "Terms of Service",
+                                href: "/tos",
+                            },
+                            {
+                                label: "Privacy Statement",
+                                href: "/privacy",
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} CMC, Inc.`,
+            },
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula,
+            },
+            scripts: ["https://kit.fontawesome.com/9201d8e0d6.js"],
+        }),
 }
 
 export default config
