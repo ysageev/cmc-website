ebb058766a182a7487e601b402e62e0a87f2c89f
ysageev <ysageev@yahoo.com>
2025-04-18 11:45:55 -0400

    New distressed asset icon

--- src/pages/main/Services.jsx
+++ src/pages/main/Services.jsx
@@ -4,39 +4,39 @@ import clsx from "clsx"
 
 function Services() {
     return (
-        <div className="page-section">
-            <div className="page-section-header">Our Services</div>
+        <div className='page-section'>
+            <div className='page-section-header'>Our Services</div>
 
-            <div className="service-items-list">
+            <div className='service-items-list'>
                 <ServicesListItem
-                    icon="fa-helmet-safety"
-                    title="Construction Loan Risk Management"
-                    details="Reduce construction loan risk"
-                    url="/services#risk-man"
+                    icon='fa-helmet-safety'
+                    title='Construction Loan Risk Management'
+                    details='Reduce construction loan risk'
+                    url='/services#risk-man'
                 />
                 <ServicesListItem
-                    icon="fa-kit fa-propinspect"
-                    title="Property Inspection and Due Diligence"
-                    details="Understand what you have."
-                    url="/services#prop-insp"
+                    icon='fa-kit fa-propinspect'
+                    title='Property Inspection and Due Diligence'
+                    details='Understand what you have.'
+                    url='/services#prop-insp'
                 />
                 <ServicesListItem
-                    icon="fa-kit fa-portfolio"
-                    title="Portfolio Management"
-                    details="All your projects, one screen."
-                    url="/services#port-man"
+                    icon='fa-kit fa-portfolio'
+                    title='Portfolio Management'
+                    details='All your projects, one screen.'
+                    url='/services#port-man'
                 />
                 <ServicesListItem
-                    icon="fa-landmark-flag"
-                    title="Historic Preservation"
-                    details="NPS Compliance and Tax Credits."
-                    url="/services#historic"
+                    icon='fa-landmark-flag'
+                    title='Historic Preservation'
+                    details='NPS Compliance and Tax Credits.'
+                    url='/services#historic'
                 />
                 <ServicesListItem
-                    icon="fa-kit fa-distressasset"
-                    title="Distressed Assets and Loan Workout"
-                    details="Right the ship."
-                    url="/services#distressed"
+                    icon='fa-kit fa-asset-distress'
+                    title='Distressed Assets and Loan Workout'
+                    details='Right the ship.'
+                    url='/services#distressed'
                 />
             </div>
         </div>
@@ -45,15 +45,15 @@ function Services() {
 
 function ServicesListItem({ icon, title, details, url }) {
     return (
-        <div className="service-item-container">
+        <div className='service-item-container'>
             <Link href={url}>
                 <i className={clsx("fa-solid fa-3x fa-fw", icon, "service-item-icon")} />
             </Link>
-            <div className="service-item-link">
+            <div className='service-item-link'>
                 <Link href={url}>
-                    <div className="service-item-title">{title}</div>
+                    <div className='service-item-title'>{title}</div>
                 </Link>
-                <div className="service-item-details">{details}</div>
+                <div className='service-item-details'>{details}</div>
             </div>
         </div>
     )
