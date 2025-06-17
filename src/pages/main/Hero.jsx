1ee5743e819fa2d4d23de03fba5ca01895a5b49c
ysageev <ysageev@yahoo.com>
2025-06-17 15:56:27 -0400

    new brochure

--- src/pages/main/Hero.jsx
+++ src/pages/main/Hero.jsx
@@ -6,45 +6,45 @@ import "./css/hero.css"
 function Hero() {
     return (
         <div>
-            <div className="page-header">
-                <div className="hero-logos">
+            <div className='page-header'>
+                <div className='hero-logos'>
                     <img
-                        src="img/cmc-logo-blue-long.svg"
-                        alt="cmc logo"
-                        className="cmc-logo"
+                        src='img/cmc-logo-blue-long.svg'
+                        alt='cmc logo'
+                        className='cmc-logo'
                     />
                     <img
-                        src="img/eoc-new-min.png"
-                        alt="eyes-on-construction"
-                        className="eyes-on-img"
+                        src='img/eoc-new-min.png'
+                        alt='eyes-on-construction'
+                        className='eyes-on-img'
                     />
                 </div>
-                <div className="page-header-text hero-header-text">Reduce Risk, Lend Confidently</div>
+                <div className='page-header-text hero-header-text'>Reduce Risk, Lend Confidently</div>
 
-                <div className="page-header-details-text">
+                <div className='page-header-details-text'>
                     For over 35 years, CMC has partnered with banks and investors in commercial construction lending to provide clarity and confidence.
                     <br />
                     <br />
                     We empower our clients to effectively measure, assess, and mitigate construction loan risks.
                 </div>
-                <div className="hero-buttons">
+                <div className='hero-buttons'>
                     <Link
-                        className="button button--primary button--lg shrink-button"
-                        to="/docs/public/introvideo"
+                        className='button button--primary button--lg shrink-button'
+                        to='/docs/public/introvideo'
                     >
-                        Video <i className="fa-duotone fa-solid fa-video" />
+                        Video <i className='fa-duotone fa-solid fa-video' />
                     </Link>
 
                     <a
-                        className="button button--primary button--lg shrink-button"
-                        href="pdf/cmc-services-brochure.pdf"
-                        rel="noreferrer"
-                        target="_blank"
+                        className='button button--primary button--lg shrink-button'
+                        href='pdf/company-overview.pdf'
+                        rel='noreferrer'
+                        target='_blank'
                     >
-                        Fact Sheet <i className="fa-duotone fa-solid fa-download" />
+                        Fact Sheet <i className='fa-duotone fa-solid fa-download' />
                     </a>
                 </div>
-                <div className="hero-scroller">
+                <div className='hero-scroller'>
                     <CompaniesUsing />
                 </div>
             </div>
